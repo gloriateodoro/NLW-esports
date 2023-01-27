7c02f0199b065c738307aad7ed9343970b328452
Gloria Teodoro <gloriateodoro1@gmail.com>
2023-01-27 09:50:57 -0300

    feat: add server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,36 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient()
 
-app.get('/ads', (req, res) => {
+// HTTP methods / API RESTful
+
+app.get('/games', async (req, res) => {
+  const games = await prisma.game.findMany()
+
+
+  return res.json(games);
+});
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+  // const gameId = req.params.id;
+
   return res.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
     { id: 3, name: 'Anúncio 3' },
   ])
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+  // const adId = req.params.id;
+
+  return res.json([])
 })
 
 app.listen(3333)
